e7ec98926c29e4e0af61e421cc74dcf568e7ffc9
OttoFidelis <ottofidelis@gmail.com>
2025-05-04 19:06:08 -0300

    criada uma entidade do banco de dados a partir do NinjaModel

---
+++ src/main/java/dev/javajavations/cadastro_de_ninjas/NinjaModel.java
@@ -0,0 +1,59 @@
+package dev.javajavations.cadastro_de_ninjas;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_ninja")
+public class NinjaModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel(){}
+
+    public NinjaModel(String nome, String email, int idade){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
