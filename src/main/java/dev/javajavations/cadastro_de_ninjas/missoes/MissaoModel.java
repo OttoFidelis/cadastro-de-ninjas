1978ff5aa3705871125ccab083898bcb362493eb
OttoFidelis <ottofidelis@gmail.com>
2025-05-04 20:28:05 -0300

    implementação do lombok concluída

--- src/main/java/dev/javajavations/cadastro_de_ninjas/missoes/MissaoModel.java
+++ src/main/java/dev/javajavations/cadastro_de_ninjas/missoes/MissaoModel.java
@@ -7,13 +7,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="tb_missao")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissaoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,39 +28,4 @@ public class MissaoModel {
 
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninjas;
-
-    public MissaoModel(){}
-    
-    public MissaoModel(Long id, String nome, String dificuldade, String status) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getDificuldade() {
-        return dificuldade;
-    }
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-    public String getStatus() {
-        return status;
-    }
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
 }
--- src/main/java/dev/javajavations/cadastro_de_ninjas/ninjas/NinjaModel.java
+++ src/main/java/dev/javajavations/cadastro_de_ninjas/ninjas/NinjaModel.java
@@ -10,9 +10,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="tb_ninja")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NinjaModel {
     
     @Id
@@ -25,45 +31,5 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missao_id")
     private MissaoModel missao;
-
-    public NinjaModel(){}
-
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
     
 }
